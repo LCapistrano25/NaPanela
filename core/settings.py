2114cd65d8c410053379fb889020ad79e9a9f46a
LCapistrano25 <leonardo.sousa@fourmaq.com.br>
2024-11-01 19:13:12 +0000

    Salvando alterações locais em settings.py

--- core/settings.py
+++ core/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-w$n203ax_do_4$g@^+!e$v1#jy_c_2fr!vj=z^i$!o_1!uo5d)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["LCapistran25.pythonanywhere.com"]
 
 
 # Application definition
@@ -89,7 +89,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'NaPanela$LCapistran25',  # Nome do banco de dados
         'USER': 'LCapistran25',  # Seu nome de usuário
-        'PASSWORD': 'Capistrano@445',  # Sua senha 
+        'PASSWORD': 'Capistrano@445',  # Sua senha
         'HOST': '10.0.4.226',  # Host do MySQL
     }
 }
