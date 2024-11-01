bd205190a3ea04c6462fd56f443b5875015247ae
LCapistrano25 <leonardo.sousa@fourmaq.com.br>
2024-11-01 16:18:30 -0300

    Modificações

--- users/admin.py
+++ users/admin.py
@@ -1,17 +1,51 @@
 from django.contrib import admin
-
-from core.mixins import SaveModelMixin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import Group
 from users.models import User
+from core.mixins import SaveModelMixin
 
-class UserAdmin(SaveModelMixin, admin.ModelAdmin):
-    list_display = ['first_name', 'email']
-    search_fields = ['first_name', 'email']
+class UserAdmin(SaveModelMixin, BaseUserAdmin):
+    # Campos exibidos na lista de usuários
+    list_display = [
+        'id', 'username', 'first_name', 'last_name', 'email',
+        'is_active', 'is_staff', 'is_superuser', 'last_login', 'date_joined'
+    ]
+    search_fields = ['username', 'first_name', 'last_name', 'email']
+    list_filter = ['is_staff', 'is_superuser', 'is_active']
     list_per_page = 10
 
+    # Configuração dos campos no formulário de edição
     fieldsets = (
         (None, {
-            'fields': ('first_name', 'email', 'password', 'is_active', 'is_staff', 'is_superuser')
+            'fields': ('username', 'password')
+        }),
+        ('Informações Pessoais', {
+            'fields': ('first_name', 'last_name', 'email')
+        }),
+        ('Permissões', {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
+            'classes': ('collapse',),
+        }),
+        ('Datas Importantes', {
+            'fields': ('last_login', 'date_joined')
         }),
     )
 
+    # Filtros horizontais para grupos e permissões
+    filter_horizontal = ('groups', 'user_permissions')
+
+    # Formulário de adição de novo usuário
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'password1', 'password2', 'first_name', 'last_name', 'email', 'is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
+        }),
+    )
+
+    readonly_fields = ['last_login', 'date_joined']
+    
+# Registra o modelo User com a customização
 admin.site.register(User, UserAdmin)
+
+# Remove o modelo Group do admin caso não seja necessário individualmente
+admin.site.unregister(Group)
